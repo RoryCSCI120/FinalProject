fecaae1698a29674cc40c0590a4f8e87e1bf1b59
RoryCSCI120 <131007848+RoryCSCI120@users.noreply.github.com>
2024-05-03 19:18:01 -0400

    Add files via upload

--- Final_Project.sql
+++ Final_Project.sql
@@ -1,52 +1,76 @@
---Demographics - Median Income Data
-CREATE TABLE demographics_median_income (
-INCOMEID SERIAL PRIMARY KEY --Create a primary key
-NAME VARCHAR(255) -- Census Tract
-STATE VARCHAR(255)
-COUNTY VARCHAR(255)
-MEDIAN_INCOME NUMERIC -- Median Household Income Value
-GEOM GEOMETRY(polygon, 4326) -- WGS 84
-);
+CREATE EXTENSION postgis
+CREATE EXTENSION postgis_raster
 
---Demographics - Language Data
-CREATE TABLE demographics_language (
-LANGUAGEID SERIAL PRIMARY KEY --Create a primary key
-NAME VARCHAR(255) -- Census Tract
-STATE VARCHAR(255)
-COUNTY VARCHAR(255)
-LANGUAGE_COUNT NUMERIC -- number of people with low english ability
-LANGUAGE_PERCENT NUMERIC -- percent of people with low english ability
-GEOM GEOMETRY(polygon, 4326) -- WGS 84
-);
+SELECT * FROM nonenglish_speakers;
 
---Buildings Data
-CREATE TABLE buildings (
-BUILDINGID SERIAL PRIMARY KEY
-TYPE VARCHAR(225)
-STREET_ID SERIAL)
-STREET_TEX VARCHAR (225)
-GEOM GEOMETRY(polygon, 4326) -- WGS 84
-);
+SELECT * FROM temp_january;
 
---Redlining Data
-CREATE TABLE redlining (
-REDLININGID SERIAL PRIMARY KEY
-ZONENUMBER VARCHAR (255) -- numbers assigned to redlining zones
-ZONECATEGORY VARCHAR (255) -- colors assigned to redlining zones
-GEOM GEOMETRY(polygon, 4326) -- WGS 84
-);
+SELECT * FROM ndvi_january;
 
---Water Data
-CREATE TABLE water (
-WATERID SERIAL PRIMARY KEY
-TYPE VARCHAR (255)
-NAME VARCHAR (255)
-GEOM GEOMETRY(polygon, 4326) -- WGS 84
-);
+SELECT * FROM water;
 
---Open Spaces Data
-CREATE TABLE open_spaces (
-OPENSPACESID SERIAL PRIMARY KEY --Create a primary key
-STREET VARCHAR (255) --location
-SIZE VARCHAR (255)
-);
+SELECT * FROM spatial_ref_sys;
+
+SELECT * FROM redlining_zones;
+
+SELECT * FROM ndvi_july;
+
+ALTER TABLE redlining_zones
+DROP COLUMN zonecat;
+
+SELECT * FROM temp_july;
+
+SELECT * FROM buildings;
+
+SELECT * FROM median_household_income;
+--so I can see all of my initial tables first	
+	
+-- Calculating the average temperature and NDVI in July in each census tract in Worcester
+SELECT nonenglish_speakers.c16001_c_4 AS nonenglish_speaker_percentage,
+AVG(ST_Value(temp_july.rast, 1, ST_Centroid(nonenglish_speakers.geom))) - 273.15 AS average_july_temperatures, -- averages the temperature values within the census tracts and converts the temperature values from Kelvin to Celcius
+AVG(ST_Value(ndvi_july.rast, 1, ST_Centroid(nonenglish_speakers.geom))) AS average_july_ndvi, -- averages the NDVI values within the census tracts
+ST_AsText(nonenglish_speakers.geom) AS geometry -- assigns the gemetry, the shape part of the shapefile
+FROM nonenglish_speakers 
+JOIN temp_july 
+ON ST_Intersects(nonenglish_speakers.geom, temp_july.rast)
+JOIN ndvi_july 
+ON ST_Intersects(nonenglish_speakers.geom, ndvi_july.rast) -- above joins the datasets together to create the final output
+GROUP BY nonenglish_speakers.c16001_c_4, nonenglish_speakers.geom; -- determines which geometry to use and which category to go by
+
+--Calculating the average temperature and NDVI in January in each Worcester Census Tract
+SELECT nonenglish_speakers.c16001_c_4 AS nonenglish_speaker_percentage,
+AVG(ST_Value(temp_january.rast, 1, ST_Centroid(nonenglish_speakers.geom))) - 273.15 AS average_january_temperatures, -- averages the temperature values within the census tracts and converts the temperature values from Kelvin to Celcius
+AVG(ST_Value(ndvi_january.rast, 1, ST_Centroid(nonenglish_speakers.geom))) AS average_january_ndvi,
+ST_AsText(nonenglish_speakers.geom) AS geometry
+FROM nonenglish_speakers
+JOIN temp_january 
+ON ST_Intersects(nonenglish_speakers.geom, temp_january.rast)
+JOIN ndvi_january 
+ON ST_Intersects(nonenglish_speakers.geom, ndvi_january.rast)
+GROUP BY nonenglish_speakers.c16001_c_4, nonenglish_speakers.geom;
+
+--Calculating the average percentage non-English Speaking Population in each Historic Redlining Zone
+WITH average_nonenglish_zones AS ( -- creates a common table expression
+SELECT rz.zonenumber, -- selects which column in the redlining zones table to use as a foundation
+AVG(nes.c16001_c_4) AS avg_percent_nonenglish -- calculates the average percentage of non-english peakers column from the initial nonenglish_speakers table
+FROM redlining_zones rz 
+LEFT JOIN nonenglish_speakers nes ON ST_Intersects(rz.geom, nes.geom) -- joins the new columns
+GROUP BY rz.zonenumber -- tells how to groupd the new columns with the old one that was used as the foundation
+)
+SELECT -- selects the columns which will be in the final csv
+rz.gid,
+rz.zonenumber,
+rz.zonecolor,
+anz.avg_percent_nonenglish,
+rz.geom
+FROM redlining_zones rz
+LEFT JOIN average_nonenglish_zones anz ON rz.zonenumber = anz.zonenumber;
+
+
+ 
+
+
+
+
+
+
